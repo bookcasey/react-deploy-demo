fbb015d126d47edf712bab26b87e60b20fcc385b
Casey Olson <casey@bookcasey.com>
2022-11-23 12:16:44 -0600

    morning q&a demo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, useHistory, Switch, Route, useRouteMatch, useParams } from 'react-router-dom';
 import './App.css';
+
+// TODO: review useRouteMatch
+// TODO: review nested routes
+
+function Dinosaur() {
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    // GET request for some dino info
+    if (isNaN(parseInt(id))) {
+      history.push('/not-found');
+    }
+  }, [id, history])
+
+  return <p>ID: {id}</p>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Casey's React App!?!?
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          Homepage
+        </Route>
+        <Route path='/dinosaurs/new'>
+          Form for a new dinosaur
+        </Route>
+        <Route path='/dinosaurs/:id'>
+          <Dinosaur />
+        </Route>
+        <Route path='/dinosaurs'>
+          DINOSAUR!
+        </Route>
+        <Route>
+          404 not found
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
